62117d08344c046b401674d12de13b398446cd71
blessinganime <136854151+blessinganime@users.noreply.github.com>
2026-02-06 18:47:52 -0500

    Refactor MealPlan to extend PaymentMethod
    Refactored MealPlan class to extend PaymentMethod and updated methods accordingly.

--- src/MealPlan.java
+++ src/MealPlan.java
@@ -1,4 +1,31 @@
-package PACKAGE_NAME;
+public class MealPlan extends PaymentMethod{
+    public MealPlan(String accountHolder, double balance){
+        super(accountHolder, balance);
+    }
 
-public class MealPlan {
+    @Override
+    public void validateAccount(){
+        if(balance < 0) {
+            System.out.println("Account not valid");
+        }
+    }
+
+    public void processPayment(double amount){
+        validateAccount();
+
+        if(amount > balance){
+            System.out.println("Transaction declined.");
+        }
+        else{
+            balance -= amount;
+            totalTransactions++;
+            System.out.println("Sucess!");
+        }
+    }
+
+    @Override
+    public String getPaymentStatus() {
+        return "Successful!";
+    }
+
 }
