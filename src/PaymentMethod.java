8b12aab89ba920bf20a0bf4f6b5d1c87db459948
blessinganime <136854151+blessinganime@users.noreply.github.com>
2026-02-06 18:49:34 -0500

    Refactor PaymentMethod to be an abstract class
    Refactor PaymentMethod class to be abstract and implement Payable interface.

--- src/PaymentMethod.java
+++ src/PaymentMethod.java
@@ -1,4 +1,13 @@
-package PACKAGE_NAME;
+public abstract class PaymentMethod implements Payable {
+    protected  String accountHolder; // using protected varable so they can only be accessed in this class
+    protected double balance;
 
-public class PaymentMethod {
+    static int totalTransactions = 0; // since no payment has been process its set to zero
+
+    public PaymentMethod(String accountHolder, double balance){ // passing in my parameter to store temporary data
+        this.accountHolder = accountHolder; // storing a copy of parameters values into instance object variables
+        this.balance = balance;
+    }
+
+    abstract void validateAccount();
 }
