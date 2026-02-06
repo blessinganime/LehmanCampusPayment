4021c76c242d83ef288404353c84c31d2ff5bb1e
blessinganime <136854151+blessinganime@users.noreply.github.com>
2026-02-06 17:04:06 -0500

    Extend PaymentMethod in CreditCard class

--- src/CreditCard.java
+++ src/CreditCard.java
@@ -1,4 +1,34 @@
-package PACKAGE_NAME;
+public class CreditCard extends PaymentMethod{
+    private double creditLimit;
 
-public class CreditCard {
+    public CreditCard(double creditLimit, String accountHolder, double balance){ // since am extending payment method am calling on its objects,
+        // hence why I passed in it in as a parameter as accountHolder (name) is needed in a credit card
+        //we also need to know the credi card limit and how much money is on the card balance
+        super(accountHolder, balance); // calls their value into this class
+        this.creditLimit = creditLimit; // storing the data of my parameter intomy instance object variable
+    }
+
+    public void processPayment(double amount){ // passing amount as a parameter for my method
+        if(amount > balance + creditLimit){// checking if there enough of money in the card for what ever purchase
+            System.out.println("Transaction Declined."); // amount either exceed credit limits or the amount of money in
+            // the card is to low
+        }
+        else{
+            balance -= amount; // if the amount of purchase is less than money in card deduct money
+            // from credit card
+            totalTransactions++; //incrementing amount of transactions, which so far is one
+        }
+
+
+    }
+
+    @Override
+    public String getPaymentStatus() {
+        return "Sucessful";
+    }
+
+    @Override
+    public void validateAccount() {
+
+    }
 }
