f18cdb78f69e19b439966dbd8a9b772f35fe7eba
blessinganime <136854151+blessinganime@users.noreply.github.com>
2026-02-06 18:46:55 -0500

    Implement payment processing with ArrayList
    Added paymentQueue to manage CreditCard and MealPlan payments.

--- src/Main.java
+++ src/Main.java
@@ -1,4 +1,17 @@
-package PACKAGE_NAME;
-
+import java.util.ArrayList;
 public class Main {
+    public static void main(String[] args){
+        ArrayList<Payable> paymentQueue = new ArrayList<>();
+        CreditCard C = new CreditCard(1000.0, "Blessing", 600.0);
+        MealPlan M = new MealPlan("Iva", 200.0);
+
+        paymentQueue.add(C);
+        paymentQueue.add(M);
+
+        for(Payable money : paymentQueue){
+            money.processPayment(50.0);
+            System.out.println(PaymentMethod.totalTransactions); // I dont think my output is supposed to look like this
+            // am not sure what am doing wrong?
+        }
+    }
 }
